f80ebf64c96e37f42fca9c04c082bc114be5cee3
Antonio <antonio.alm_pdf@hotmail.com>
2020-02-28 00:25:15 -0300

    Queue - Fila Duplamente Encadeada Circular in Python OPP

---
+++ queue - fila/venv/QueuePy.py
@@ -0,0 +1,46 @@
+from Node import Node
+
+class QueuePy:
+
+    def __int__(self):
+        self.head = Node();
+        self.syrup = Node();
+        self.head.set_prox(self.syrup);
+        self.head.set_ant(self.syrup);
+        self.syrup.set_prox(self.head);
+        self.syrup.set_ant(self.head);
+
+    def empty(self):
+        if(self.head.get_prox() == self.syrup):
+            return True;
+        return False;
+
+    def enqueue(self, value, key):
+        novo = Node();
+        novo.set_key(key);
+        novo.set_value(value);
+        if(self.empty()):
+            self.head.set_prox(novo);
+            novo.set_ant(self.head);
+            novo.set_prox(self.syrup);
+            self.syrup.set_ant(novo);
+        else:
+            aux = self.syrup.get_ant();
+            aux.set_prox(novo);
+            novo.set_ant(aux);
+            novo.set_prox(self.syrup);
+            self.syrup.set_ant(novo);
+
+    def dequeue(self):
+        aux = self.head.get_prox();
+        aux2 = aux.get_prox();
+        self.head.set_prox(aux2);
+        aux2.set_ant(self.head);
+        del aux;
+
+    def printQe(self):
+        aux = self.head.get_prox();
+        while(aux != self.syrup):
+            print(aux.get_value(), " ");
+            aux = aux.get_prox();
+        print("OLa Mundo!");
---
+++ queue - fila/venv/main.py
@@ -0,0 +1,13 @@
+from QueuePy import QueuePy
+
+queue = QueuePy();
+queue.enqueue(25, 1);
+queue.enqueue(30, 2);
+queue.enqueue(10, 3);
+queue.enqueue(15, 4);
+queue.enqueue(32, 5);
+queue.enqueue(74, 6);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.printQe();
